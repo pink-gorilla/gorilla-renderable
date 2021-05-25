ac8299bacb994637b44086409a7e7b3015e8da34
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-05-25 03:38:22 -0500

    command clear-all

--- src/picasso/document/commands.cljs
+++ src/picasso/document/commands.cljs
@@ -12,6 +12,11 @@
    (rf/dispatch [:doc/exec [:eval-all]])))
 
 (rf/reg-event-fx
+ :notebook/clear-all
+ (fn [_ _]
+   (rf/dispatch [:doc/exec [:clear-all]])))
+
+(rf/reg-event-fx
  :segment-active/kernel-toggle
  (fn [_ _]
    (rf/dispatch [:doc/exec [:kernel-toggle-active]])))
